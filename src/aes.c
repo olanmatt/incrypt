b035711f6bf7a7947fd6490e415a5ad1d2ae2888
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2015-05-28 21:45:22 -0400

    New code is also std=c89 compatible.

--- src/aes.c
+++ src/aes.c
@@ -18,8 +18,8 @@
 #include <string.h>
 #include <aes.h>
 
-static inline void
-add_round_key(uint32_t *restrict state, const uint32_t *restrict round_key)
+static void
+add_round_key(uint32_t* state, const uint32_t* round_key)
 {
     state[0] ^= round_key[0];
     state[1] ^= round_key[1];
@@ -27,7 +27,7 @@ add_round_key(uint32_t *restrict state, const uint32_t *restrict round_key)
     state[3] ^= round_key[3];
 }
 
-static inline void
+static void
 sub_bytes(uint32_t *state, const sbox_t sb)
 {
     state[0] = SBOX_AT(state[0], sb);
@@ -47,11 +47,12 @@ mult(uint8_t a, uint8_t b)
 static void
 inv_mix_columns(uint32_t *state)
 {
-    for (int i = 0; i < 4; i++) {
+    int i;
+    for (i = 0; i < 4; i++) {
         uint8_t st[] = {state[i] >> 24, state[i] >> 16, state[i] >> 8,
                         state[i]};
         uint8_t tmp[] = {
-            // XXX: if I don't cast to uint8_t, values are wrong.
+            /* XXX: if I don't cast to uint8_t, values are wrong. */
             (uint8_t)(mult(st[0], 0xe) ^ mult(st[1], 0xb) ^ mult(st[2], 0xd) ^
                       mult(st[3], 0x9)),
             (uint8_t)(mult(st[0], 0x9) ^ mult(st[1], 0xe) ^ mult(st[2], 0xb) ^
@@ -69,24 +70,22 @@ inv_mix_columns(uint32_t *state)
 static void
 mix_columns(uint32_t *state)
 {
-    // clang-format off
-    for (int i = 0; i < 4; i++) {
+    int i;
+    for (i = 0; i < 4; i++) {
         uint8_t s[] = {state[i] >> 24, state[i] >> 16, state[i] >> 8, state[i]};
         uint8_t m[] = {mult2[s[0]],    mult2[s[1]],    mult2[s[2]],   mult2[s[3]]};
 
-        // mt[x] ^ st[x] == mult(st[x] , 3)
+        /* mt[x] ^ st[x] == mult(st[x] , 3) */
         state[i] = (m[0]      ^ m[1]^s[1] ^ s[2]      ^ s[3])      << 24 |
                    (s[0]      ^ m[1]      ^ m[2]^s[2] ^ s[3])      << 16 |
                    (s[0]      ^ s[1]      ^ m[2]      ^ m[3]^s[3]) << 8 |
                    (m[0]^s[0] ^ s[1]      ^ s[2]      ^ m[3]);
     }
-    // clang-format on
 }
 
 static void
 shift_rows(uint32_t *state)
 {
-    // clang-format off
     uint32_t tmp[] = {
         (state[0] & 0xff000000) | (state[1] & 0x00ff0000) |
         (state[2] & 0x0000ff00) | (state[3] & 0x000000ff),
@@ -100,7 +99,6 @@ shift_rows(uint32_t *state)
         (state[3] & 0xff000000) | (state[0] & 0x00ff0000) |
         (state[1] & 0x0000ff00) | (state[2] & 0x000000ff),
     };
-    // clang-format on
 
     state[0] = tmp[0];
     state[1] = tmp[1];
@@ -111,7 +109,6 @@ shift_rows(uint32_t *state)
 static void
 inv_shift_rows(uint32_t *state)
 {
-    // clang-format off
     uint32_t tmp[] = {
         (state[0] & 0xff000000) | (state[3] & 0x00ff0000) |
         (state[2] & 0x0000ff00) | (state[1] & 0x000000ff),
@@ -125,7 +122,6 @@ inv_shift_rows(uint32_t *state)
         (state[3] & 0xff000000) | (state[2] & 0x00ff0000) |
         (state[1] & 0x0000ff00) | (state[0] & 0x000000ff),
     };
-    // clang-format on
 
     state[0] = tmp[0];
     state[1] = tmp[1];
@@ -136,9 +132,10 @@ inv_shift_rows(uint32_t *state)
 void
 aes128_expand_key(const uint8_t *keytext, uint32_t *key)
 {
+    uint8_t i, j;  /* using uint8 for better packing */
     STR_TO_WORD_ARRAY(keytext, key);
 
-    for (uint8_t i = 1, j = 4; i < 11; i++, j += 4) {
+    for (i = 1, j = 4; i < 11; i++, j += 4) {
         key[j] = SBOX_AT(LSHIFT(key[j - 1], 1), sbox) ^ (RCON[i] << 24) ^ key[j - 4];
 
         key[j + 1] = key[j - 3] ^ key[j];
@@ -151,10 +148,11 @@ void
 aes128_encrypt_block(const uint8_t* input, const uint32_t* key, uint8_t* output)
 {
     uint32_t plaintext[4];
+    int i;
     STR_TO_WORD_ARRAY(input, plaintext);
 
     add_round_key(plaintext, &key[0]);
-    for (int i = 4; i < 40; i += 4) {
+    for (i = 4; i < 40; i += 4) {
         shift_rows(plaintext);
 
         sub_bytes(plaintext, sbox);
@@ -172,11 +170,12 @@ void
 aes128_decrypt_block(const uint8_t* input, const uint32_t* key, uint8_t* output)
 {
     uint32_t ciphertext[4];
+    int i;
     STR_TO_WORD_ARRAY(input, ciphertext);
 
     add_round_key(ciphertext, &key[40]);
 
-    for (int i = 36; i > 0; i -= 4) {
+    for (i = 36; i > 0; i -= 4) {
         inv_shift_rows(ciphertext);
 
         sub_bytes(ciphertext, rev_sbox);
